e41c2a0cd4982f419e7f1566fd4c5f5b2be1a3e8
Zhengguan Dai <35508409+zd6@users.noreply.github.com>
2020-03-22 21:53:13 -0500

    Delete nUpdateWaitbar.m

--- nUpdateWaitbar.m
+++
@@ -1,8 +0,0 @@
-function nUpdateWaitbar(maxr)
-    global p N h r_prev
-    if r_prev < maxr
-        r_prev = maxr;
-    end
-    waitbar(min(p/N, 1.0),h,sprintf('Iterations: %d/%d, current r_{max} = %5f',p, N, r_prev));
-    p = p + 1;
-end
